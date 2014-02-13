db0da150729a1a9b67affaf1f67622c06253bf81
Petar Tsankov <ptsankov@inf.ethz.ch>
2014-02-13 15:29:12 +0100

    adding a modified pexpect version

--- src/xsb.py
+++ src/xsb.py
@@ -1,8 +1,8 @@
-import pexpect
 from parser import Parser
 from config import config
 from atom import Atom
 import sys
+import pexpect
 
 class XSB:
     
@@ -20,17 +20,25 @@ class XSB:
         
     def loadBellogProgram(self, filename):        
         bellogRules = Parser.parseFile(filename)
+        
+        # check if there are any predicates that have not been defined
         undefinedAtoms = Atom.SYMBOLS - {r.head.pred for r in bellogRules}.union({'false', 'true', 'top', 'bot'})
         if len(undefinedAtoms) > 0:
             print 'The following predicate symbols have not been defined:', ', '.join(undefinedAtoms)
             sys.exit(-1)
+        
+        # translate the rules to Datalog and load them into XSB    
         self.xsb.sendline('[user].')
         self.xsb.sendline(':- auto_table.')
         for bellogRule in bellogRules:
             for datalogRule in bellogRule.toDatalogRules():
                 self.xsb.sendline(datalogRule + '.')
+                
+        # load also the static datalog rules
         for datalogRule in XSB.STATIC_RULES:           
             self.xsb.sendline(datalogRule + '.')
+            
+        # tell XSB that we're done loading rules
         self.xsb.sendcontrol('d')    
         self.xsb.expect('yes')
         print 'File', filename, 'loaded'
