d58572f27fbcc3b163db7d2130dbeba711375be3
Petar Tsankov <petar.tsankov@gmail.com>
2014-02-16 18:35:56 +0100

    !

--- src/runWeb.py
+++ src/runWeb.py
@@ -4,25 +4,27 @@ import sys
 from xsb import XSB
 
 def main():  
-    bellogString = None
+    policyString = None
     queryString = None    
     opts, args = getopt.getopt(sys.argv[1:], 'i:q:', ['input', 'query'])
     for o, a in opts:
         if o == '-i':
-            bellogString = a
+            policyString = a
         elif o == '-q':
             queryString = a
             
-    if bellogString is None or queryString is None:
+    if policyString is None or queryString is None:
         print "Incorrect usage"
         sys.exit(-1)
-    xsb = XSB(  )
+
+    xsb = XSB()
     try:
-        xsb.loadBellogProgram([x for x in bellogString.split('<next>') if x != ''])
-        print xsb.query(queryString)
+        policy = Policy.fromString(policyString)
     except Exception as e:
         print 'Error:', e
         sys.exit(-1)     
+    xsb.loadPolicy(policy)
+    print xsb.query(queryString)
     xsb.close()
 
 if __name__ == '__main__':
