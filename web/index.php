49e73bda026ed959b5a9d271280ee48693a962fe
Petar Tsankov <ptsankov@tor.ethz.ch>
2014-02-17 13:24:33 +0100

    web

--- web/index.php
+++ web/index.php
@@ -15,15 +15,18 @@ polPiet(carol,lab) :- true\n\
 polAnn(carol,lab) :- false\n\
 polAnn(dave,lab) :- true\n\
 owner(carol) :- true";
-function loadPolicy(pol) {
+var req1 = "p(a,c)";
+var req2 = "pol(carol,lab)";
+function loadPolicy(pol, req) {
   document.getElementById("policy").value = pol;
-  document.getElementById("query").value = '';
+  document.getElementById("query").value = req;
+  document.getElementById("output").innerHTML= "";
 }
 </script>
 <?php 
   echo '<table style="width:300px" id="table">';
   echo '<form action="index.php" method="post">';
-  echo '<tr><td>';
+  echo '<tr class="title"><td>';
   echo '<h1>BelLog Policy Interpreter</h1></td></tr><tr><td>';
   if ($_POST['policy']) {
     $policy = $_POST["policy"];
@@ -37,29 +40,29 @@ function loadPolicy(pol) {
   }
   echo '</textarea>';
   echo '</td><td valign="top">';
-  echo '<input type="button" class="gray" name="loadEx1" value="Load Example 1" onClick="loadPolicy(example1);">';
-  echo '<input type="button" class="gray" name="loadEx2" value="Load Example 2" onClick="loadPolicy(example2);">';
-  echo '<br></td></tr><tr><td>';
-  echo '<p>The request </p><input id="query" type="text" name="query" placeholder="Type a request" ';
+  echo '<input type="button" class="gray" name="loadEx1" value="Example 1" onClick="loadPolicy(example1, req1);">';
+  echo '<input type="button" class="gray" name="loadEx2" value="Example 2" onClick="loadPolicy(example2, req2);">';
+  echo '<br></td></tr><tr class="request"><td>';
+  echo '<br><br><p class="reqtext">Request</p></td></tr>';
+  echo '<tr><td><br><input id="query" type="text" name="query" placeholder="Type a request" ';
   if ($query) {
     echo "value=$query>";
   } else {
     echo "value=''>";
   }
-  echo '<input type="submit" class="gray" value="evaluates">';
-  echo '<p> to : </p>';
+  echo '<input type="submit" class="gray" value="evaluate"></td></tr><tr><td><br></td></tr>';
   if ($query && $policy) {
     $policyFormatted = preg_replace('/\n+/', "<newline>", trim($policy));
     $ret = shell_exec("python src/runWeb.py -i \"$policyFormatted\" -q \"$query\" 2>&1");
     $length = strlen('Error');
     if (substr($ret, 0, $length) === 'Error') {
-      echo "<br><br><p style='color:red'>$ret</p>";
+      echo "<tr class='error' id='output'><td>$ret</td></tr>";
     } else {
-      echo "<p id='result'>$ret</p><br><br>";
+      echo "<tr class='result' id='output'><td>$ret</td></tr>";
     }
   }
-  echo '</td></tr><tr><td>';
-  echo '<br><br><p>For details on BelLog\'s syntax see: <a href="http://github.com/ptsankov/bellog/">http://github.com/ptsankov/bellog/</a></p>';
+  echo '<tr><td>';
+  echo '<br><br><br><br><br><hr/><p>For details on BelLog\'s syntax see: <a href="http://github.com/ptsankov/bellog/">http://github.com/ptsankov/bellog/</a></p>';
   echo '</td></tr></table>';
   echo '</form>';
 ?>
